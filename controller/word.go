1eac2f5e0380af0917d5ce8a0fbd862afb879b3c
pongsakon <pongsakonpromma@gmail.com>
2018-11-26 19:08:12 +0700

    กราบบบ

--- controller/word.go
+++ controller/word.go
@@ -72,7 +72,8 @@ func Word(context *gin.Context) {
 		text :=strings.Split(tag.msg, " ")
 		if (strings.Compare(text[0], "มี") == 0 || strings.Compare(text[len(input)-1], "ไหม") == 0){
 
-			test :="----------------------------/nNJ Modem & Router/n----------------------------/nรายการสินค้า/nAsus a550      X1               2000 บาท/nรวม : 2000   บาท/n----------------------------"
+			test :="----------------------------/n"+"NJ Modem & Router/n"+"----------------------------/n"+"รายการสินค้า/n"+"Asus a550      X1               2000 บาท/n"+"รวม : 2000   บาท/n----------------------------"
+
 			ans = model.Answer{
 				"คำถามต่อเนื่อง",
 				test,
