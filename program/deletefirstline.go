0cd02b759ecf73e5d8117a750cec2f08ea80f88e
pongsakon <pongsakonpromma@gmail.com>
2019-01-16 16:39:09 +0700

    ลองใช้ไลเบอร์ลี่ที่ต้องใช้ในการทำงาน

---
+++ program/deletefirstline.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
+func PopLine(f *os.File) ([]byte, error) {
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+	buf := bytes.NewBuffer(make([]byte, 0, fi.Size()))
+
+	_, err = f.Seek(0, os.SEEK_SET)
+	if err != nil {
+		return nil, err
+	}
+	_, err = io.Copy(buf, f)
+	if err != nil {
+		return nil, err
+	}
+	line, err := buf.ReadString('\n')
+	if err != nil && err != io.EOF {
+		return nil, err
+	}
+
+	_, err = f.Seek(0, os.SEEK_SET)
+	if err != nil {
+		return nil, err
+	}
+	nw, err := io.Copy(f, buf)
+	if err != nil {
+		return nil, err
+	}
+	err = f.Truncate(nw)
+	if err != nil {
+		return nil, err
+	}
+	err = f.Sync()
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = f.Seek(0, os.SEEK_SET)
+	if err != nil {
+		return nil, err
+	}
+	return []byte(line), nil
+}
+
+func main() {
+	fname := `popline.txt`
+	f, err := os.OpenFile(fname, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	line, err := PopLine(f)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("pop:", string(line))
+}
---
+++ program/sqltocsv.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"WebService/controller"
+	"bytes"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql" // or the driver of your choice
+	"github.com/joho/sqltocsv"
+	"io"
+	"os"
+)
+
+
+func PopLine(f *os.File) ([]byte, error) {
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+	buf := bytes.NewBuffer(make([]byte, 0, fi.Size()))
+
+	_, err = f.Seek(0, os.SEEK_SET)
+	if err != nil {
+		return nil, err
+	}
+	_, err = io.Copy(buf, f)
+	if err != nil {
+		return nil, err
+	}
+	line, err := buf.ReadString('\n')
+	if err != nil && err != io.EOF {
+		return nil, err
+	}
+
+	_, err = f.Seek(0, os.SEEK_SET)
+	if err != nil {
+		return nil, err
+	}
+	nw, err := io.Copy(f, buf)
+	if err != nil {
+		return nil, err
+	}
+	err = f.Truncate(nw)
+	if err != nil {
+		return nil, err
+	}
+	err = f.Sync()
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = f.Seek(0, os.SEEK_SET)
+	if err != nil {
+		return nil, err
+	}
+	return []byte(line), nil
+}
+func main(){
+	db, err := sql.Open("mysql", controller.DATABASE)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	rows, _ := db.Query("SELECT * FROM menu")
+
+	err = sqltocsv.WriteFile("./program/report.csv", rows)
+
+	//fname := `report.csv`
+	f, err := os.OpenFile("./program/report.csv", os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	line, err := PopLine(f)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("pop:", string(line))
+
+	if err != nil {
+		panic(err)
+	}else{
+		fmt.Println("เสร็จแล้ว")
+	}
+
+}
