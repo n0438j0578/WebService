789645822c14b8ca303f9116877714d8a7b53c38
pongsakon <pongsakonpromma@gmail.com>
2019-05-27 14:29:51 +0700

    เพิ่มคำว่า คับ

--- program/cutwordthai.go
+++ program/cutwordthai.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"WebService/test"
 	"fmt"
 	"github.com/narongdejsrn/go-thaiwordcut"
 
@@ -10,23 +9,8 @@ import (
 func main() {
 	segmenter := gothaiwordcut.Wordcut()
 	segmenter.LoadDefaultDict()
-	//result := segmenter.Segment("ช่วยแนะนำเร้าเตอร์ที่ส่งสัญญาณ5Ghzได้หน่อยค่ะ")
-	result := segmenter.Segment("แบบเดิม")
+	result := segmenter.Segment("รองเท้ากัดค่ะขอเปลี่ยนได้ไหมคะ")
 	fmt.Println(result)
-	//fmt.Println(strings.ToLower("Linksys LSS-EA9300-AH Max-Stream AC4000 Tri-Band Wi-Fi Router"))
-	//var validID = regexp.MustCompile(`[0-9]\s*\.*\**\\*\t*\n*\r*:\s*\.*\**\\*\t*\n*\r*[0-9]`)
-	//ปป
-	//fmt.Println(validID.MatchString("123				:  123123"))
-	//
-	//words := strings.Fields("123:123123")
-	//for i:=0;i< len(words);i++  {
-	//	fmt.Println(words[i])
-	//}
-	//fmt.Println(len(strings.Split("123				:  123123", ":")))
-
-
-	fmt.Println(test.CheckBank("เลขธนาคาร"))
-
 }
 
 
